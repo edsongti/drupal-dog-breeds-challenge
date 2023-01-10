ad25cb9331a723c19e59448284ad3079d7437b97
Edson Carlos de Oliveira <eolive49@its.jnj.com>
2023-01-10 11:09:25 -0300

    Refactor Custom Service

--- web/modules/custom/dog_breeds/src/Services/BreedImageService.php
+++ web/modules/custom/dog_breeds/src/Services/BreedImageService.php
@@ -2,40 +2,67 @@
 
 namespace Drupal\dog_breeds\Services;
 
+use Drupal\Component\DependencyInjection\ContainerInterface;
+use Drupal\Core\Logger\LoggerChannelFactoryInterface;
+use GuzzleHttp\ClientInterface;
 use Exception;
 
 /**
  * Class BreedImageService
- * @package Drupal\mymodule\Services
+ * @package Drupal\dog_breeds\Services
  */
 class BreedImageService {
 
     /**
-     * BreedImageService constructor.
-     * @param AccountInterface $currentUser
+     * @var \GuzzleHttp\ClientInterface
      */
-    public function __construct() { }
-
+    protected $httpClient;
 
     /**
-     * @return \Drupal\Component\Render\MarkupInterface|string
+     * Logger Factory.
+     *
+     * @var \Drupal\Core\Logger\LoggerChannelFactoryInterface
      */
-    public static function getBreedImages($breedSlug) {
+    protected $loggerFactory;
+
+    /**
+     * BreedImageService constructor.
+     * @param \GuzzleHttp\ClientInterface $http_client
+     *   A Guzzle client object.
+     * @param \Drupal\Core\Logger\LoggerChannelFactoryInterface$loggerFactory
+     */
+    public function __construct(ClientInterface $http_client, LoggerChannelFactoryInterface $loggerFactory) {
+        $this->httpClient = $http_client;
+        $this->loggerFactory = $loggerFactory->get('dog_breeds');
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public static function create(ContainerInterface $container) {
+        return new static(
+            $container->get('http_client'),
+            $container->get('logger.factory')
+        );
+    }
+
+    /**
+     * @return array|log
+     */
+    public function getBreedImages($breedSlug) {
 
         //call api via service API
         $api_url = "https://dog.ceo/api/breed/{$breedSlug}/images/random";
-        $client = \Drupal::httpClient();
 
         try {
+            $request = $this->httpClient->get($api_url);
 
-        $request = $client->get($api_url);
-
-        $response = json_decode($request->getBody());
-        if ($response->status == "success") {
-            return $response->message;
-        }
+            $response = json_decode($request->getBody());
+            if ($response->status == "success") {
+                return $response->message;
+            }
         } catch(Exception $e) {
-
+            $this->loggerFactory->info($e->getMessage());
         }
     }
 
