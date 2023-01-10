e7ffe08d3b71a25551746fa58aa69e84b223e573
Edson Carlos de Oliveira <eolive49@its.jnj.com>
2023-01-10 20:46:45 -0300

    Fix phpcs issues

--- web/modules/custom/dog_breeds/src/Services/BreedImageService.php
+++ web/modules/custom/dog_breeds/src/Services/BreedImageService.php
@@ -5,65 +5,71 @@ namespace Drupal\dog_breeds\Services;
 use Drupal\Component\DependencyInjection\ContainerInterface;
 use Drupal\Core\Logger\LoggerChannelFactoryInterface;
 use GuzzleHttp\ClientInterface;
-use Exception;
 
 /**
- * Class BreedImageService
+ * Class BreedImageService.
+ *
+ * Service to get a dog breed image from the API.
+ *
  * @package Drupal\dog_breeds\Services
  */
 class BreedImageService {
 
-    /**
-     * @var \GuzzleHttp\ClientInterface
-     */
-    protected $httpClient;
+  /**
+   * Guzzle\Client instance.
+   *
+   * @var \GuzzleHttp\ClientInterface
+   */
+  protected $httpClient;
 
-    /**
-     * Logger Factory.
-     *
-     * @var \Drupal\Core\Logger\LoggerChannelFactoryInterface
-     */
-    protected $loggerFactory;
+  /**
+   * Logger Factory.
+   *
+   * @var \Drupal\Core\Logger\LoggerChannelFactoryInterface
+   */
+  protected $loggerFactory;
 
-    /**
-     * BreedImageService constructor.
-     * @param \GuzzleHttp\ClientInterface $http_client
-     *   A Guzzle client object.
-     * @param \Drupal\Core\Logger\LoggerChannelFactoryInterface$loggerFactory
-     */
-    public function __construct(ClientInterface $http_client, LoggerChannelFactoryInterface $loggerFactory) {
-        $this->httpClient = $http_client;
-        $this->loggerFactory = $loggerFactory->get('dog_breeds');
-    }
-
-    /**
-     * {@inheritdoc}
-     */
-    public static function create(ContainerInterface $container) {
-        return new static(
-            $container->get('http_client'),
-            $container->get('logger.factory')
-        );
-    }
-
-    /**
-     * @return array|log
-     */
-    public function getBreedImages($breedSlug) {
-
-        //call api via service API
-        $api_url = "https://dog.ceo/api/breed/{$breedSlug}/images/random";
-
-        try {
-            $request = $this->httpClient->get($api_url);
-
-            $response = json_decode($request->getBody());
-            if ($response->status == "success") {
-                return $response->message;
-            }
-        } catch(Exception $e) {
-            $this->loggerFactory->info($e->getMessage());
-        }
-    }
-
+  /**
+   * {@inheritdoc}
+   */
+  public function __construct(ClientInterface $http_client, LoggerChannelFactoryInterface $logger_factory) {
+    $this->httpClient = $http_client;
+    $this->loggerFactory = $logger_factory->get('dog_breeds');
   }
+
+  /**
+   * {@inheritdoc}
+   */
+  public static function create(ContainerInterface $container) {
+    return new static(
+          $container->get('http_client'),
+          $container->get('logger.factory')
+      );
+  }
+
+  /**
+   * Method to get a dog breed image from the API.
+   *
+   * @return array|log
+   *   Will return an array or a log in the system.
+   */
+  public function getBreedImages($breedSlug) {
+
+    // Call api via service API.
+    $api_url = "https://dog.ceo/api/breed/{$breedSlug}/images/random";
+
+    try {
+      $request = $this->httpClient->get($api_url);
+
+      $response = json_decode($request->getBody());
+      if ($response->status == "success") {
+        return $response->message;
+      }
+    }
+    catch (\Exception $e) {
+
+      $this->loggerFactory->info($e->getMessage());
+    }
+  }
+
+}
